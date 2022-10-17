8a568a4d6b15c90a76fc86d493ffe35c5873439c
JouJou <joujou@pop-os.localdomain>
2022-10-16 22:30:24 -0300

    Adding user/me Router and Adding first Repository

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,28 +1,37 @@
 import { connection } from "../database/db.js";
+import * as userRepository from "../repositories/usersRepository.js";
 
 export async function getUser(req, res) {
   try {
     const { user } = res.locals;
-    const { rows: checkUser } = await connection.query(
-      `SELECT * FROM users WHERE id = $1`,
-      [user.id]
-    );
+    const { rows: checkUser } = await userRepository.checkUser(user.id);
     if (checkUser.length === 0) {
       return res.sendStatus(404);
     }
-    const { rows: userInfo } = await connection.query(
-      `SELECT users.id, users.name, SUM(links."visitsCount") AS "visitsCount", json_agg(json_build_object(
- 	 'id', links.id,
-	 'shortUrl', links."shortUrl",
-	 'url', links.url,
-	 'visitsCount', links."visitsCount"
-    )) AS "shortenedUrls"
-    FROM users JOIN links ON users.id = links."userId"
-    WHERE users.id = $1
-    GROUP BY users.id`,
-      [user.id]
-    );
-    res.status(200).send(userInfo);
+
+    const { rows: userInfo } = await userRepository.getUserInfo(user.id);
+
+    if (userInfo.length === 0) {
+      const { rows: secondaryInfo } = await userRepository.getSecondaryInfo(
+        user.id
+      );
+      const userObject = {
+        id: secondaryInfo[0].id,
+        name: secondaryInfo[0].name,
+        visitCount: 0,
+        shortenedUrls: [],
+      };
+
+      return res.status(200).send(userObject);
+    }
+    return res.status(200).send(userInfo);
+  } catch {
+    return res.sendStatus(500);
+  }
+}
+
+export async function getRanking(req, res) {
+  try {
   } catch {
     return res.sendStatus(500);
   }
---
+++ src/repositories/usersRepository.js
@@ -0,0 +1,24 @@
+import { connection } from "../database/db.js";
+
+export async function getUserInfo(id) {
+  return connection.query(
+    `SELECT users.id, users.name, SUM(links."visitsCount") AS "visitsCount", json_agg(json_build_object(
+ 	 'id', links.id,
+	 'shortUrl', links."shortUrl",
+	 'url', links.url,
+	 'visitsCount', links."visitsCount"
+    )) AS "shortenedUrls"
+    FROM users JOIN links ON users.id = links."userId"
+    WHERE users.id = $1
+    GROUP BY users.id`,
+    [id]
+  );
+}
+
+export async function getSecondaryInfo(id) {
+  return connection.query(`SELECT id, name FROM users WHERE id = $1`, [id]);
+}
+
+export async function checkUser(id) {
+  return connection.query(`SELECT * FROM users WHERE id = $1`, [id]);
+}
