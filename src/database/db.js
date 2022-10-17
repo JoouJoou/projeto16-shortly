7910fd6f09650bf85e9986b04e781464552395da
JouJou <joujou@pop-os.localdomain>
2022-10-17 00:40:35 -0300

    Ready for deploy

--- src/database/db.js
+++ src/database/db.js
@@ -5,14 +5,11 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-const user = "postgres";
-const password = "1234";
-const host = "localhost";
-const port = 5432;
-const database = "shortly";
-
 const connection = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export { connection };
--- src/server.js
+++ src/server.js
@@ -12,5 +12,5 @@ app.use(express.json());
 app.use(router);
 
 app.listen(process.env.PORT || 4000, () => {
-  console.log("Listen on 4000");
+  console.log("Server running on port " + process.env.PORT);
 });
